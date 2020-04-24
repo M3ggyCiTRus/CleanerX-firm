8deadbeeb476f3438970102e7962c71d565a1718
luc_ha <49131729+lucsco@users.noreply.github.com>
2020-04-24 12:45:08 +0200

    Update fsperm.h

--- arm9/source/filesys/fsperm.h
+++ arm9/source/filesys/fsperm.h
@@ -18,7 +18,7 @@
 #define PERM_XORPAD     (1UL<<12) // can't be enabled, placeholder
 #define PERM_CART       (1UL<<13)
 #define PERM_VRAM       (1UL<<14) // can't be enabled, placeholder
-#define PERM_BASE       (PERM_SDCARD | PERM_IMAGE | PERM_RAMDRIVE | PERM_EMU_LVL0 | PERM_SYS_LVL0)
+#define PERM_BASE       (PERM_SDCARD | PERM_IMAGE | PERM_RAMDRIVE | PERM_EMU_LVL1 | PERM_SYS_LVL3 | PERM_SDDATA)
 
 // permission levels / colors
 #define PERM_BLUE       (GetWritePermissions()&PERM_MEMORY)
